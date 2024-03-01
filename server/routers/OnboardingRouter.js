5e70974fd384f515972db785404fcc60daa71cf8
Ychen463 <ychen463@gatech.edu>
2024-03-01 08:45:03 -0500

    change onboarding router for api

--- server/routers/OnboardingRouter.js
+++ server/routers/OnboardingRouter.js
@@ -1,14 +1,7 @@
 import express from 'express';
 import {
-<<<<<<< HEAD
   applyUserOnboarding, getUserOnboarding, hrUpdateDecision,
   viewOnboardingApplicationsByStatus, getAllTokens,
-=======
-  applyUserOnboarding,
-  getUserOnboarding,
-  hrUpdateDecision,
-  viewOnboardingApplicationsByStatus,
->>>>>>> d06cdbdbabf5acea40123bd387ff865e16a7a258
 } from '../controllers/OnboardingController.js';
 import jwtVerifyToken from '../middlewares/AuthMiddleware.js';
 import checkRole from '../middlewares/CheckRoleMiddleware.js';
@@ -23,26 +16,10 @@ router.get(
   getUserOnboarding
 );
 // HR
-<<<<<<< HEAD
 router.patch('/onboarding/:userAccountId/status', jwtVerifyToken, checkRole(['HR']), hrUpdateDecision);
 
 // Search/Filter all emps by Statuses
 router.get('/onboardings/status', jwtVerifyToken, checkRole(['HR']), viewOnboardingApplicationsByStatus);
 router.get('/onboardings', jwtVerifyToken, checkRole(['HR']), getAllTokens);
-=======
-router.patch(
-  '/onboarding/:userAccountId/status',
-  jwtVerifyToken,
-  checkRole(['HR']),
-  hrUpdateDecision
-);
-// Search/Filter all emps by Statuses
-router.get(
-  '/onboardings/status',
-  jwtVerifyToken,
-  checkRole(['HR']),
-  viewOnboardingApplicationsByStatus
-);
->>>>>>> d06cdbdbabf5acea40123bd387ff865e16a7a258
 
 export default router;
