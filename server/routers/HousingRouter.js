f789a9d38f47b63e1fd4fb54d16f3f9c5316bfb5
Jiaqi Zhang <jzhang2239@gmail.com>
2024-03-02 20:59:19 -0800

    FIX: change the order of routes defined in router to avoid request getting into wrong routes
    `summary` was recognized as `houseID`

--- server/routers/HousingRouter.js
+++ server/routers/HousingRouter.js
@@ -15,15 +15,15 @@ const router = express.Router();
 // get the information of the house that the user is assigned to with a user’s _id
 router.get('/housing/user', jwtVerifyToken, getUserHousing);
 
+// get summary information of all houses
+// may need pagination, but currently not
+router.get('/housing/summary', jwtVerifyToken, checkRole(['HR']), getHousesSummary);
+
 // get a house's full information with its _id
 router.get('/housing/:houseID', jwtVerifyToken, getHouseInfo);
 
 // create a new house with basic house information:
 // a name, an address, landlord information, capacity basic facility information
 router.post('/housing', jwtVerifyToken, checkRole(['HR']), createNewHouse);
-
-// get summary information of all houses
-// may need pagination, but currently not
-router.get('/housing/summary', jwtVerifyToken, checkRole(['HR']), getHousesSummary);
 
 export default router;
