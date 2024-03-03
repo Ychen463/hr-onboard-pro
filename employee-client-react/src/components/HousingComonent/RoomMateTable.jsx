86c0ad20da85899feeea5ad17a64d38e1a9b297b
Darren <darrenslion@gmail.com>
2024-03-03 15:05:46 -0800

    fixed roommate tab name not matching

--- employee-client-react/src/components/HousingComonent/RoomMateTable.jsx
+++ employee-client-react/src/components/HousingComonent/RoomMateTable.jsx
@@ -11,9 +11,9 @@ import {
   Box,
 } from '@mui/material';
 
-function RoommateTable({ roommates, index }) {
+function RoommateTable({ roommates }) {
   // Replace this with your actual data fetching logic
-
+  console.log('in table', roommates);
   return (
     <Box>
       <Typography variant="h6" gutterBottom component="div" sx={{ p: 2, textAlign: 'left' }}>
@@ -23,18 +23,18 @@ function RoommateTable({ roommates, index }) {
         <Table aria-label="roomMateTable">
           <TableHead>
             <TableRow>
-              <TableCell> Name</TableCell>
-              <TableCell align="left">Phone Number</TableCell>
+              <TableCell> Full Name</TableCell>
+              <TableCell align="left">Email</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {roommates.map((roommate) => (
+            {roommates?.map((roommate) => (
               <TableRow key={roommate.phone}>
                 <TableCell component="th" scope="row">
                   {/* <Avatar alt={roommate.name} src={roommate.avatar} sx={{ mr: 2, display: 'inline-flex', verticalAlign: 'middle' }} /> */}
-                  {roommate.name}
+                  {`${roommate.userFirstName} ${roommate.userLastName}`}
                 </TableCell>
-                <TableCell align="left">{roommate.phone}</TableCell>
+                <TableCell align="left">{roommate.email}</TableCell>
               </TableRow>
             ))}
           </TableBody>
